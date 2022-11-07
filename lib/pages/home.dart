cacf5c9f1d6fc8b48127b877ac6af6391636a5eb
xaiur <davinoverclock@gmail.com>
2022-11-07 20:07:14 +0800

    Faker

---
+++ lib/pages/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:faker/faker.dart';
+
+class HomePage extends StatelessWidget {
+  var faker = new Faker();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Faker"),
+      ),
+      body: ListView.builder(
+        itemCount: 35,
+        itemBuilder: (context, index) => ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey.shade400,
+            // backgroundImage: NetworkImage(faker.image.image(random: true)),
+            backgroundImage: NetworkImage(
+                "https://picsum.photos/${200 + index}/${300 + index}"),
+          ),
+          title: Text("${faker.person.name()}"),
+          subtitle: Text("${faker.internet.email()}"),
+        ),
+      ),
+    );
+  }
+}
