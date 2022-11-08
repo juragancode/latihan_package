0a138f77c22b5d480c6ef2a5562ebed24404b89c
xaiur <davinoverclock@gmail.com>
2022-11-09 01:10:09 +0800

    Dropdown Search

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
-import './pages/intro.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +12,62 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: IntroductionPage(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  List<Map<String, dynamic>> dataList = [
+    {
+      "country": "Brazil",
+      "id": 1,
+    },
+    {
+      "country": "Italia",
+      "id": 2,
+    },
+    {
+      "country": "Tunisia",
+      "id": 3,
+    },
+    {
+      "country": "Canada",
+      "id": 4,
+    },
+    {
+      "country": "Rusia",
+      "id": 5,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Dropdown Search"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: DropdownSearch<Map<String, dynamic>>(
+          mode: Mode.MENU,
+          items: dataList,
+          // popupItemDisabled: (String s) => s.startsWith('I'),
+          onChanged: (value) => print(value?["id"] ?? null),
+          selectedItem: {
+            "country": "Brazil",
+            "id": 1,
+          },
+          showSearchBox: true,
+          // showClearButton: true,
+          popupItemBuilder: (context, item, isSelected) => ListTile(
+            title: Text(item["country"].toString()),
+          ),
+          dropdownBuilder: (context, selectedItem) => Text(
+              selectedItem?["country"].toString() ?? "Please choise country!"),
+        ),
+      ),
     );
   }
 }
